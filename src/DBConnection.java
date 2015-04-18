a4b580d6ccc245b989720e6758d7ed5856a4136b
suryaharshan1 <suryaharshan1@gmail.com>
2015-04-19 02:41:36 +0530

    Completes DBConnection and prints output on Console

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -65,11 +65,13 @@ public class DBConnection {
 		Statement stmt,stmt1;
 		ResultSet resultSet,resultSet2;
 		Connection connection[] = new Connection[4];
+		System.out.println("********** CONNECTION LOG **********");
+		System.out.println();
 		connection[0] = DBConnection.connectDB("jdbc:mysql://127.0.0.1:3306/","DDBProject","root","");
 		connection[1] = DBConnection.connectDB("jdbc:mysql://127.0.0.1:3306/","DDBProject1","root","");
 		connection[2] = DBConnection.connectDB("jdbc:mysql://127.0.0.1:3306/", "DDBProject2", "root", "");
 		connection[3] = DBConnection.connectDB("jdbc:mysql://127.0.0.1:3306/", "DDBProject3", "root", ""); 
-		
+		System.out.println();
 		/*try {
 			stmt = conn1.createStatement();
 			rs = stmt.executeQuery("SELECT * FROM faculty_master");
@@ -85,7 +87,7 @@ public class DBConnection {
 		}*/
 		
 		CCJSqlParserManager pm = new CCJSqlParserManager();
-		  String sql = "SELECT Book.name,Publisher.name,Customer.name,Orders.quantity FROM Publisher,Book,Customer,Orders WHERE Publisher.publisherId = Book.Publisher_publisherId AND Publisher.publisherId = 10 AND Customer.customerId = Orders.Customer_customerId AND Orders.Book_bookId = Book.bookId AND Book.bookId = 12 AND Customer.rank >= 2";
+		  String sql = "SELECT Book.title,Publisher.name,Customer.name,Orders.quantity FROM Publisher,Book,Customer,Orders WHERE Publisher.publisherId = Book.Publisher_publisherId AND Publisher.publisherId = 10 AND Customer.customerId = Orders.Customer_customerId AND Orders.Book_bookId = Book.bookId AND Book.bookId = 12 AND Customer.rank >= 2";
 		  Select statement = (Select) pm.parse(new StringReader(sql));
 		  /* 
 		  now you should use a class that implements StatementVisitor to decide what to do
@@ -98,8 +100,13 @@ public class DBConnection {
 		  	QueryTree queryTree = new QueryTree();
 		  	queryTree.genSelectTree(selectStatement);
 		  	queryTree.generateTreeList();
+		  	System.out.println("********** QUERY TREE **********");
+		  	System.out.println();
 		  	queryTree.displayTree();
+		  	System.out.println();
 		  	
+		  	System.out.println("********** LOCAL QUERIES **********");
+		  	System.out.println();
 		  	List<LeafNode> leaves = queryTree.getLeafNodeList();
 		  	for (int i = 0; i < leaves.size(); i++) {
 		  		String query = new String();
@@ -124,9 +131,11 @@ public class DBConnection {
 					node = node.getParentNode();
 				}
 				query += ";";
+				System.out.println("***** QUERY "+ (i+1) +" *****");
 				System.out.println(query);
-				
+				System.out.println();
 				check.displayConditionList();
+				System.out.println();
 				
 				stmt = connection[0].createStatement();
 				resultSet = stmt.executeQuery(
@@ -135,7 +144,61 @@ public class DBConnection {
 						where + space + tableName + space + equals + space + "\"" + leaves.get(i).getTableName() + "\"" + ";"
 						);
 				
-				ArrayList<Integer> sites = new ArrayList<Integer>();
+				if (!resultSet.isBeforeFirst() ) {    
+					try (java.sql.PreparedStatement s1 = connection[1].prepareStatement(query);
+						     ResultSet rs = s1.executeQuery()) {
+						    ResultSetMetaData meta = rs.getMetaData();
+						    
+						    
+						    StringBuilder columnNames = new StringBuilder();
+						    StringBuilder bindVariables = new StringBuilder();
+						    
+						    List<String> columns = new ArrayList<>();
+						    for (int j = 1; j <= meta.getColumnCount(); j++)
+						        columns.add(meta.getColumnName(j));
+
+						    for (String column : columns) {
+						        if (columnNames.length() > 0) {
+						            columnNames.append(", ");
+						            bindVariables.append(", ");
+						        }
+
+						        columnNames.append(column);
+						        bindVariables.append('?');
+						    }
+
+						    String query2 = "INSERT INTO " + leaves.get(i).getTableName() + " ("
+						               + columnNames
+						               + ") VALUES ("
+						               + bindVariables
+						               + ")";
+						    
+						    /*
+						    PreparedStatement s2 = connection[0].prepareStatement(
+						        "INSERT INTO " + leaves.get(i).getTableName() + " ("
+						      + columns.stream().collect(Collectors.joining(", "))
+						      + ") VALUES ("
+						      + columns.stream().map(c -> "?").collect(Collectors.joining(", "))
+						      + ")");
+							*/
+						    
+						    java.sql.PreparedStatement s2 = connection[0].prepareStatement(query2);
+						    
+						    while (rs.next()) {
+						        for (int j = 1; j <= meta.getColumnCount(); j++)
+						            s2.setObject(j, rs.getObject(j));
+
+						        s2.addBatch();
+						    }
+
+						    s2.executeBatch();
+						}
+ 
+					 continue;
+				} 
+				
+				boolean flag = false;
+				System.out.println("***** FRAGMENTATION CONDITIONS *****");
 				while(resultSet.next()){
 					String id = resultSet.getString(siteID);
 					String attrName = resultSet.getString(attributeName);
@@ -146,6 +209,7 @@ public class DBConnection {
 										startValue + ": " + start + comma + space +
 										endValue + ": " + end);
 					if(conditions.contains(attrName)){
+						flag = true;
 						if(check.isRange(resultSet.getString(attributeName), resultSet.getInt(startValue), resultSet.getInt(endValue))){
 								//System.out.println(rs.getString(siteID));
 								//stmt1 = connection[resultSet.getInt(siteID)].createStatement();
@@ -203,36 +267,84 @@ public class DBConnection {
 								//just ignore case - as no site contains required information
 						}
 					}
-					else{
-						
+				}
+				System.out.println();
+				if(!flag){
+					for(int site = 1; site <= 3; site++){
+						try (java.sql.PreparedStatement s1 = connection[site].prepareStatement(query);
+							     ResultSet rs = s1.executeQuery()) {
+							    ResultSetMetaData meta = rs.getMetaData();
+							    
+							    
+							    StringBuilder columnNames = new StringBuilder();
+							    StringBuilder bindVariables = new StringBuilder();
+							    
+							    List<String> columns = new ArrayList<>();
+							    for (int j = 1; j <= meta.getColumnCount(); j++)
+							        columns.add(meta.getColumnName(j));
+	
+							    for (String column : columns) {
+							        if (columnNames.length() > 0) {
+							            columnNames.append(", ");
+							            bindVariables.append(", ");
+							        }
+	
+							        columnNames.append(column);
+							        bindVariables.append('?');
+							    }
+	
+							    String query2 = "INSERT INTO " + leaves.get(i).getTableName() + " ("
+							               + columnNames
+							               + ") VALUES ("
+							               + bindVariables
+							               + ")";
+							    
+							    /*
+							    PreparedStatement s2 = connection[0].prepareStatement(
+							        "INSERT INTO " + leaves.get(i).getTableName() + " ("
+							      + columns.stream().collect(Collectors.joining(", "))
+							      + ") VALUES ("
+							      + columns.stream().map(c -> "?").collect(Collectors.joining(", "))
+							      + ")");
+								*/
+							    
+							    java.sql.PreparedStatement s2 = connection[0].prepareStatement(query2);
+							    
+							    while (rs.next()) {
+							        for (int j = 1; j <= meta.getColumnCount(); j++)
+							            s2.setObject(j, rs.getObject(j));
+	
+							        s2.addBatch();
+							    }
+	
+							    s2.executeBatch();
+							}
 					}
 				}
-				
-				
-			}
-		  	
-		  	
-		  	
-		  	/*------where clause----*/
-		  	
-			WhereItemsFinder finder3 = new WhereItemsFinder(selectStatement);
-			
-			/*------join clause----*/
-			ArrayList<JoinNode> joins = new ArrayList<JoinNode>();
-			ArrayList<JoinExpression> joinList = finder3.getJoinList();
-			for(int i=0;i<joinList.size();++i){
-				JoinNode node2 = new JoinNode();
-				//System.out.println(joinList.get(i).leftTableName);
-				node2.setRightTableName(joinList.get(i).rightTableName);
-				//System.out.println(joinList.get(i).rightTableName);
-				
-				node2.addAttribute(joinList.get(i).leftColumn, joinList.get(i).rightColumn);
-				;
-				
-			}
-			
+			}		
 		  }
-		
-		
+		  resultSet = connection[0].createStatement().executeQuery(sql);
+		  System.out.println();
+		  System.out.println("********** RESULT **********");
+		  System.out.println();
+		  int columnCount = resultSet.getMetaData().getColumnCount();
+		  for (int j = 1; j <= columnCount; j++) {
+			System.out.print(resultSet.getMetaData().getColumnName(j) + "\t");
+		}
+		  System.out.println();
+		  while(resultSet.next()){
+			  for (int j = 1; j <= columnCount; j++) {
+				System.out.print(resultSet.getString(j) + "\t");
+			}
+			  System.out.println();
+		  }
+		  
+		  connection[0].createStatement().executeUpdate("DELETE FROM Book");
+		  connection[0].createStatement().executeUpdate("DELETE FROM Publisher");
+		  connection[0].createStatement().executeUpdate("DELETE FROM Customer");
+		  connection[0].createStatement().executeUpdate("DELETE FROM Orders");
+		  for (int j = 0; j < 4; j++) {
+			  connection[j].close();
+		}
 	} 
 }
